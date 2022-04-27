5d12395d4abaa3526ea54fb3bf46fd2fbaff4c3f
Jhonata Polito <jhonata.demuner@gmail.com>
2022-04-27 20:13:53 -0300

    Atualização dos nomes de arquivos

---
+++ ex_9/ex9_code.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#  20221bsi0080.py
+#
+#  Copyright 2022 Jhonata Polito Demuner
+#
+
+n = int(0);
+ident = str ('');
+quant = int(0);
+mat = str ("");
+freq = str ('');
+
+n = int(input());
+
+ident = (input());
+quant = int(input());
+mat = input();
+freq = input();
+
+while n 
+
