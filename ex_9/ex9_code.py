971f458d0657758c37aac13d0e8e7b55b433ccd7
Jhonata Polito <jhonata.demuner@gmail.com>
2022-04-28 18:54:42 -0300

    Ex9 add

--- ex_9/ex9_code.py
+++ ex_9/ex9_code.py
@@ -3,20 +3,64 @@
 #  20221bsi0080.py
 #
 #  Copyright 2022 Jhonata Polito Demuner
-#
 
-n = int(0);
-ident = str ('');
-quant = int(0);
-mat = str ("");
-freq = str ('');
+def main():
 
-n = int(input());
+    #! Declaração de Variáveis
+    n = int(0)
+    ident = str('')
+    quant = int(0)
+    mat = str('')
+    freq = str('')
+    freqperc = float(0.0)
+    maiorperc = float(0.0)
+    menorperc = float(0.0)
+    turmamaiorperc = float(0.0)
+    turmamenorperc = float(0.0)
+    countaus = int(0)
+    ausacima = int(0)
+    i = int(0)
+    j = int(0)
 
-ident = (input());
-quant = int(input());
-mat = input();
-freq = input();
+    #! Inicialização de Variáveis
+    n = int(input())
+    maiorperc = -1
+    menorperc = 101
 
-while n 
+    #! Entrada de dados + Processamento
+    for i in range(n):
+        ident = input()
+        quant = int(input())
+        freqperc = 0
+        countaus = 0
 
+        for j in range(quant):
+            mat = input()
+            freq = input()
+
+            if freq == 'A':
+                countaus += 1
+            freqperc = countaus / quant * 100
+
+            if freqperc > maiorperc:
+                maiorperc = freqperc
+                turmamaiorperc = ident
+
+            if freqperc < menorperc:
+                menorperc = freqperc
+                turmamenorperc = ident
+
+        if freqperc > 20:
+            ausacima += 1
+
+        #! Saída de Dados
+        print(f'TURMA={ident} AUSENCIA={freqperc:.2f}%')
+    print(
+        f'TURMA COM MAIOR PORCENTAGEM DE AUSENCIA={turmamaiorperc} AUSENCIA={maiorperc:.2f}%')
+    print(
+        f'TURMA COM MENOR PORCENTAGEM DE AUSENCIA={turmamenorperc} AUSENCIA={menorperc:.2f}%')
+    print(f'{ausacima} TURMAS COM PORCENTAGEM DE AUSENCIA SUPERIOR A 20%')
+
+
+if __name__ == '__main__':
+    main()
