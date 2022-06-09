621333a0dc9520825c46f3a17909c488aeee9989
Jhonata Polito <jhonata.demuner@gmail.com>
2022-06-08 21:29:57 -0300

    Adicionando ex16

---
+++ ex_16/20221bsi0080Q2.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+#  20221bsi0080Q2.py
+#  
+#  Copyright 2022
+#  Jhonata Polito Demuner
+#  20221bsi0080
+
+# Faça um programa em Python3 que:
+# a) Calcule o somatório dos n primeiros termos da série definida por:
+# S=1/1^3-1/3^3+1/5^3-1/7^3+1/9^3-...
+# b) Calcule o valor de Pi usando a fórmula:
+# π= S×32  3
+# onde o valor de S foi obtido no item a)
+# c) Imprima os termos da série e os valores calculados nos itens a) e b)
+# Obs.: O valor de n é um número inteiro que deverá ser informado pelo usuário e
+# deverá ser checado por seu programa, para as situações a seguir:
+# n <= 0 a saída impressa será N INVALIDO
+# n > 0 a saída impressa será composta pelos valores calculados nos itens a) e b)
+# com 5 casas decimais
+
+import modulo
+
+def main():
+
+    # Declaração de variáveis
+    n = int (0)
+    denominador = int(0)
+    numerador = int(0)
+    s = float(0)
+
+    # Entrada
+    n = int(input())
+
+    # Incialização de variáveis
+    denominador = 1
+    numerador = 1
+    nFixo = n
+
+    # Processamento
+    if n <= 0:
+        print(f'N={nFixo} N INVALIDO')
+    else:
+        s = modulo.calcTermos(numerador, denominador, n)
+        
+        pi = (s * 32) ** (1/3)
+
+        # Saída
+        print(f'N={nFixo} SOMATORIO={s:.5f} PI={pi:.5f}')
+    
+if __name__ == '__main__':
+    main()
