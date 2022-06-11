2046ca22259d73a7b49eab058e585142ec412863
Jhonata Polito <jhonata.demuner@gmail.com>
2022-06-11 00:44:51 -0300

    Update modulo.py

--- ex_16/modulo.py
+++ ex_16/modulo.py
@@ -1,12 +1,11 @@
 def calcTermos (numerador, denominador, n):
     s = int(0)
     
-    while n > 0:
+    for i in range(n, 0, -1):
         termo = numerador / denominador ** 3
         s += termo
         print(f'{numerador}/{denominador}^3')
         denominador += 2
         numerador *= -1
-        n -= 1
 
     return s;
